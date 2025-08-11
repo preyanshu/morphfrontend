ee0b9e6b6aa291517aa0eba042b7969161e838de
Preyanshu Mishra <mishrapreyanshu@gmail.com>
2025-08-12 01:25:48 +0530

    fixed typing issue

--- hr-portal/utils/addresses.ts
+++ hr-portal/utils/addresses.ts
@@ -1,4 +1,5 @@
 // Contract Addresses
-export const PAYROLL_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_PAYROLL_CONTRACT_ADDRESS;
-export const MOCK_USDC_ADDRESS = process.env.NEXT_PUBLIC_MOCK_USDC_ADDRESS;
-export const ESOP_CONTRACT_ADDRESS = process.env.NEXT_PUBLIC_ESOP_CONTRACT_ADDRESS;
+export const ESOP_CONTRACT_ADDRESS = (process.env.NEXT_PUBLIC_ESOP_CONTRACT_ADDRESS || '0x') as `0x${string}`;
+export const PAYROLL_CONTRACT_ADDRESS = (process.env.NEXT_PUBLIC_PAYROLL_CONTRACT_ADDRESS || '0x') as `0x${string}`;
+export const MOCK_USDC_ADDRESS = (process.env.NEXT_PUBLIC_MOCK_USDC_ADDRESS || '0x') as `0x${string}`;
+
